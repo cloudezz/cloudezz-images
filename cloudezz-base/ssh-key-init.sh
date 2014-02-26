93e97e037c896a3cad15fd3122edb85bd7015ea1
tanmally <tm@beyondbytes.co.in>
2014-02-26 22:03:59 +0530

    added cloudezz to sudo grp

--- cloudezz-base/ssh-key-init.sh
+++ cloudezz-base/ssh-key-init.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-ROOT_USR=root
-CLOUDEZZ_USR=cloudezz
-
-
 # This will generate a random, 8-character password:
 #ROOT_SSH_PASSWD=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c15`
 
 
 # This will actually set the password for root and cloudezz user:
 if [ $ROOT_SSH_PASSWD ]; then
-echo "Creating new user $CLOUDEZZ_USR"
-echo "Setting new ssh password for user $CLOUDEZZ_USR"
-sudo su -c "useradd $CLOUDEZZ_USR -s /bin/bash -m -g sudo -G sudo" >/dev/null 2>/dev/null
-sudo chpasswd << 'END'
-$CLOUDEZZ_USR:$ROOT_SSH_PASSWD
-END
-echo "Setting new ssh password for user $ROOT_USR"
-echo "$ROOT_USR:$ROOT_SSH_PASSWD" | chpasswd
-fi 
+echo "Creating new user cloudezz"
+echo "Setting ssh password for user cloudezz"
+sudo userdel cloudezz >/dev/null 2>/dev
+sudo su -c "useradd cloudezz -s /bin/bash -m -g sudo -G sudo" >/dev/null 2>/dev
+echo "cloudezz:$ROOT_SSH_PASSWD" | chpasswd
+echo "Setting ssh password for user root"
+echo "root:$ROOT_SSH_PASSWD" | chpasswd
+fi
+
+
 
 
 # print the private key on console so that the user can connect thru ssh 
 KEYGEN=/usr/bin/ssh-keygen
 KEYFILE=/root/.ssh/id_rsa
 
+
 if [ ! -f $KEYFILE ]; then
   $KEYGEN -q -t rsa -N "" -f $KEYFILE
   cat $KEYFILE.pub >> /root/.ssh/authorized_keys
 fi
 
+# if the root shh password is missing then print private key
+if [ ! $ROOT_SSH_PASSWD  ]; then
 echo "== Use this private key to log in =="
 cat $KEYFILE
+fi
+
