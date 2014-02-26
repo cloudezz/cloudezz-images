fe6da34ac3a956223ef97c7d8996c198fe72512e
tanmally <tm@beyondbytes.co.in>
2014-02-26 20:29:29 +0530

    Adding cloudezz user with sudo rights

--- cloudezz-base/ssh-key-init.sh
+++ cloudezz-base/ssh-key-init.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-USR=root
+ROOT_USR=root
+CLOUDEZZ_USR=cloudezz
+
 
 # This will generate a random, 8-character password:
 #ROOT_SSH_PASSWD=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c15`
 
-# This will actually set the password:
+# This will actually set the password for root and cloudezz user:
 if [ $ROOT_SSH_PASSWD ]; then
+echo "Creating new user $CLOUDEZZ_USR"
+echo "Setting new ssh password for user $CLOUDEZZ_USR"
+useradd -p $ROOT_SSH_PASSWD $CLOUDEZZ_USR >/dev/null 2>/dev/null
+adduser $CLOUDEZZ_USR sudo >/dev/null 2>/dev/null
 echo "Setting new ssh password for user $USR"
 echo "$USR:$ROOT_SSH_PASSWD" | chpasswd
 fi 
