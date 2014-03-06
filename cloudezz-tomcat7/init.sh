b5137ad6d403669eb3362d35e24bdcf696f75f41
tanmally <tm@beyondbytes.co.in>
2014-03-06 16:41:58 +0530

    getting the zip and running the war file if run build pack true

--- cloudezz-tomcat7/init.sh
+++ cloudezz-tomcat7/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-
-
 rm -r /opt/build-pack >/dev/null 2>/dev/null
-git clone https://github.com/bbytes/cloudezz.git /opt/build-pack >/dev/null
-chmod +x -R /opt/build-pack/cloudezz-build-pack 
-cd /opt/build-pack/cloudezz-build-pack/cloudezz-tomcat7-build-pack && ant 
+wget -P /opt/build-pack/ https://github.com/cloudezz/cloudezz-tomcat7-build-pack/archive/master.zip  >/dev/null 2>/dev/null
+chmod +x -R /opt/build-pack/
+unzip /opt/build-pack/master.zip -d /opt/build-pack/  >/dev/null 2>/dev/null
+chmod +x -R /opt/build-pack/ 
+cd /opt/build-pack/cloudezz-tomcat7-build-pack-master && ant 
