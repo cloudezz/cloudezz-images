d0170e23c7cd84ff535b770f5cafec39d6c242ac
tanmally <tm@beyondbytes.co.in>
2014-03-06 16:10:33 +0530

    supress error

--- cloudezz-tomcat7/init.sh
+++ cloudezz-tomcat7/init.sh
@@ -2,7 +2,7 @@
 
 
 
-rm -r /opt/build-pack >/dev/null
+rm -r /opt/build-pack >/dev/null 2>/dev/null
 git clone https://github.com/bbytes/cloudezz.git /opt/build-pack >/dev/null
 chmod +x -R /opt/build-pack/cloudezz-build-pack 
 cd /opt/build-pack/cloudezz-build-pack/cloudezz-tomcat7-build-pack && ant 
