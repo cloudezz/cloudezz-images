3120e98085d8205759e844be62e5a5e160ce9475
Thanneer <tm@beyondbytes.co.in>
2014-03-09 00:59:22 +0530

    removed sudo as it fails for root user in extended images

--- cloudezz-base/start.sh
+++ cloudezz-base/start.sh
@@ -3,13 +3,13 @@
 
 # restart Fail2Ban service to avoid dos attack thru ssh
 echo "Starting fail2ban service"
-sudo service fail2ban restart >/dev/null
+service fail2ban restart >/dev/null
 
 # start Shell in a box service only when the webshell env is set to true
 if [ $WEB_SHELL ]
 then
 echo "Starting shellinabox service"
-sudo service shellinabox reload >/dev/null
+service shellinabox reload >/dev/null
 fi
 
 
