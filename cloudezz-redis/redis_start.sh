5d212c97becf45fe8989060add9248aa37e23589
tanmally <tm@beyondbytes.co.in>
2014-03-09 22:30:19 +0530

    Fixed redis start

--- cloudezz-redis/redis_start.sh
+++ cloudezz-redis/redis_start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e 
 
-chown -R redis:redis /var/lib/redis
+chown -R root:root /var/lib/redis
 
-echo "Starting redis server..."
 if [ $REDIS_PASSWORD ]
 then
-exec sudo -u redis /usr/bin/redis-server --requirepass $REDIS_PASSWORD >/dev/null
+echo "Starting redis server with password $REDIS_PASSWORD"
+exec  /usr/bin/redis-server --requirepass $REDIS_PASSWORD >/dev/null
 else
-exec sudo -u redis /usr/bin/redis-server >/dev/null
+echo "Starting redis server..."
+exec  /usr/bin/redis-server >/dev/null
 fi
 
