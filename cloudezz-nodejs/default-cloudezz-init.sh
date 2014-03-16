a4a9c44f63d86949aa6cb282fb1a4a298e454dbc
tanmally <tm@beyondbytes.co.in>
2014-03-17 00:33:38 +0530

    Fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down

--- cloudezz-nodejs/default-cloudezz-init.sh
+++ cloudezz-nodejs/default-cloudezz-init.sh
@@ -13,5 +13,7 @@ fi
 
 # stop supervisord in daemon mode
 service supervisor stop
+# fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
+unlink /var/run/supervisor.sock
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
 supervisord -n -c /etc/supervisor.conf
