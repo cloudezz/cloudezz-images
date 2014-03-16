14ae9542b21c84782215b348dc948546834a5ffe
tanmally <tm@beyondbytes.co.in>
2014-03-16 16:49:08 +0530

    Fixed java and tmcat image with new way fo starting

--- cloudezz-tomcat7/default-cloudezz-init.sh
+++ cloudezz-tomcat7/default-cloudezz-init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#!/bin/sh
+
+
+#Copy the war file from mounted directory to tomcat webapps directory
+if [ ! -z "$WAR_URL" ]
+then
+echo "Removing old war files and copying $WAR_URL to tomcat webapps folder" 
+rm -r /var/lib/tomcat7/webapps/*
+cd /var/lib/tomcat7/webapps/  && wget -q $WAR_URL
+fi
+
 
 if [ $RUN_BUILD_PACK == "true" ]
 then
@@ -11,3 +22,5 @@ chmod +x -R /opt/build-pack/
 cd /opt/build-pack/cloudezz-tomcat7-build-pack-master && ant 
 fi
 
+echo "Starting tomcat server on port 8080"
+CMD service tomcat7 restart && tail -f /var/lib/tomcat7/logs/catalina.out
