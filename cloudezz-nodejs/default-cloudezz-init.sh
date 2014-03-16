9a69b1700a79fb87440fe1db57b46fdd0d823475
tanmally <tm@beyondbytes.co.in>
2014-03-17 02:34:03 +0530

    Fixed console error msg

--- cloudezz-nodejs/default-cloudezz-init.sh
+++ cloudezz-nodejs/default-cloudezz-init.sh
@@ -14,6 +14,6 @@ fi
 # stop supervisord in daemon mode
 service supervisor stop
 # fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
-unlink /var/run/supervisor.sock
+unlink /var/run/supervisor.sock 2> /dev/null
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
 supervisord -n -c /etc/supervisor.conf
