50308bbce051365453438a8c10bdc96401a533be
tanmally <tm@beyondbytes.co.in>
2014-03-17 02:27:35 +0530

    Fixed init

--- cloudezz-java7/default-cloudezz-init.sh
+++ cloudezz-java7/default-cloudezz-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # stop supervisord in daemon mode
-supervisord stop
+service supervisor stop
+# fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
+unlink /var/run/supervisor.sock
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
 supervisord -n -c /etc/supervisor.conf
