c37aaf034648b9d25d3b200aee4dd178eedd5374
tanmally <tm@beyondbytes.co.in>
2014-03-17 02:37:10 +0530

    Fixed console error msg

--- cloudezz-java7/default-cloudezz-init.sh
+++ cloudezz-java7/default-cloudezz-init.sh
@@ -5,4 +5,5 @@ service supervisor stop
 # fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
 unlink /var/run/supervisor.sock 2> /dev/null
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
+echo "Starting supervisord in foreground ..."
 supervisord -n -c /etc/supervisor.conf
