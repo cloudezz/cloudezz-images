d3c5a99d20975331096da9a629343592cb0c12af
tanmally <tm@beyondbytes.co.in>
2014-03-17 05:59:38 +0530

    Fixed ghost start env

--- cloudezz-ghost/default-cloudezz-init.sh
+++ cloudezz-ghost/default-cloudezz-init.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 cd /cloudezz/ghost
-sed -e '0,/host: '"'"'127.0.0.1'"'"'/s//host: '"'"'0.0.0.0'"'"'/' \
-config.example.js > config.js
 #-------------------------------------------------------------------------------
 npm config set strict-ssl false
 npm config set registry "http://registry.npmjs.org/"
 #-------------------------------------------------------------------------------
 echo "Starting ghost blogging service"
-forever -o ghost-out.log -e ghost-err.log index.js
+NODE_ENV=production forever -o ghost-out.log -e ghost-err.log index.js
 #===============================================================================
