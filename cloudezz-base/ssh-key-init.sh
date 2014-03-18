fd515a9bca19f6cc7ceb4374d352f7c672c033a1
tanmally <tm@beyondbytes.co.in>
2014-03-18 19:06:26 +0530

    Fix for ssh not working

--- cloudezz-base/ssh-key-init.sh
+++ cloudezz-base/ssh-key-init.sh
@@ -6,8 +6,8 @@
 
 # This will actually set the password for root and cloudezz user:
 if [ ! $ROOT_SSH_PASSWD ]; then
-ROOT_SSH_PASSWD=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c15`
-echo "SSH password for 'root' and 'cloudezz' user = $ROOT_SSH_PASSWD"
+  ROOT_SSH_PASSWD=`tr -dc A-Za-z0-9_ < /dev/urandom | head -c15`
+  echo "SSH password for 'root' and 'cloudezz' user = $ROOT_SSH_PASSWD"
 fi
 
 echo "Setting ssh password for user cloudezz"
@@ -21,16 +21,16 @@ echo "root:$ROOT_SSH_PASSWD" | chpasswd
 
 
 
-#	# print the private key on console so that the user can connect thru ssh 
-#	KEYGEN=/usr/bin/ssh-keygen
-#	KEYFILE=/root/.ssh/id_rsa
-#
-#
-#	if [ ! -f $KEYFILE ]; then
-#	 $KEYGEN -q -t rsa -N "" -f $KEYFILE
-#	 cat $KEYFILE.pub >> /root/.ssh/authorized_keys
-#   fi
-#
+# print the private key on console so that the user can connect thru ssh 
+KEYGEN=/usr/bin/ssh-keygen
+KEYFILE=/root/.ssh/id_rsa
+
+if [ ! -f $KEYFILE ]; then
+  $KEYGEN -q -t rsa -N "" -f $KEYFILE
+  cat $KEYFILE.pub >> /root/.ssh/authorized_keys
+  chmod 600 /root/.ssh/*
+fi
+
 #	# if the root shh password is missing then print private key
 #	if [ ! $ROOT_SSH_PASSWD  ]; then
 #	  echo "== Use this private key to log in =="
