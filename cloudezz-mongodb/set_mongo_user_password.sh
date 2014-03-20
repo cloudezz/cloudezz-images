2d2b0fca5c36d2876c3237ed4dcabf3897c2d194
tanmally <tm@beyondbytes.co.in>
2014-03-20 18:53:40 +0530

    rephrased

--- cloudezz-mongodb/set_mongo_user_password.sh
+++ cloudezz-mongodb/set_mongo_user_password.sh
@@ -12,7 +12,7 @@ sleep 3
 USER=${MONGODB_USER:-"admin"}
 PASS=${MONGODB_PASSWD:-$(pwgen -s 12 1)}
 echo "=============================================================================="
-echo "=> Creating admin privilege user '$USER' with password '$PASS' in MongoDB"
+echo "Creating admin privilege user '$USER' with password '$PASS' in MongoDB"
 echo "=============================================================================="
 mongo admin --eval "db.addUser( { user: '$USER', pwd: '$PASS', roles: [ 'userAdminAnyDatabase', 'dbAdminAnyDatabase' ] } );"
 mongo admin --eval "db.shutdownServer();"
