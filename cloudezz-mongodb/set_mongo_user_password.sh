b81acf988bab0b970d697e89687a83b68bd113ec
tanmally <tm@beyondbytes.co.in>
2014-03-20 18:41:36 +0530

    fixed syntax issue

--- cloudezz-mongodb/set_mongo_user_password.sh
+++ cloudezz-mongodb/set_mongo_user_password.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#!/bin/bash
-
 if [ -f /opt/cloudezz-config/.mongodb_user_password_set ]; then
 echo "MongoDB password already set!"
 exit 0
@@ -9,14 +7,13 @@ fi
 
 /usr/bin/mongod --smallfiles &
 sleep 3
-USER=${MONGODB_USER:"admin"}
+USER=${MONGODB_USER:-"admin"}
 PASS=${MONGODB_PASSWD:-$(pwgen -s 12 1)}
-echo "=> Creating an admin privilege user '${USER}' with '${PASS}' password in MongoDB"
+echo "=> Creating an admin privilege user '$USER' with '$PASS' password in MongoDB"
 use admin;
-db.addUser({ user:"$USER", pwd:"$PASS", roles:["dbAdminAnyDatabase","clusterAdmin"]});
+db.addUser({ user:'$USER', pwd:'$PASS', roles:["dbAdminAnyDatabase","clusterAdmin"]});
 db.shutdownServer();
 sleep 3
 
 echo "=> Done!"
 touch /opt/cloudezz-config/.mongodb_user_password_set
-
