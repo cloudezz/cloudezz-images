6993884461d319cb0eb9e005d1717422d5d13e69
tanmally <tm@beyondbytes.co.in>
2014-03-22 18:24:48 +0530

    Fixing supervisord daemon issue

--- cloudezz-java7/default-cloudezz-init.sh
+++ cloudezz-java7/default-cloudezz-init.sh
@@ -4,6 +4,9 @@
 service supervisor stop
 # fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
 unlink /var/run/supervisor.sock 2> /dev/null
+unlink /tmp/supervisor.sock 2> /dev/null
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
 echo "Starting supervisord in foreground ..."
-supervisord -n -c /etc/supervisor/supervisor.conf
+mv -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor/supervisor.conf
+mv -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor.conf
+service supervisor start
