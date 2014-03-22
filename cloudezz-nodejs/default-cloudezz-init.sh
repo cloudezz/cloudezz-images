8721fb5d16256fa05a20e79e3aa56af475429519
tanmally <tm@beyondbytes.co.in>
2014-03-22 19:24:55 +0530

    Fixing supervisord daemon issue

--- cloudezz-nodejs/default-cloudezz-init.sh
+++ cloudezz-nodejs/default-cloudezz-init.sh
@@ -11,13 +11,18 @@ cd /opt/build-pack/cloudezz-node-build-pack-master && ant
 fi
 
 
-# stop supervisord in daemon mode
-service supervisor stop
-# fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
-unlink /var/run/supervisor.sock 2> /dev/null
-unlink /tmp/supervisor.sock 2> /dev/null
-# start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
-echo "Starting supervisord in foreground ..."
-cp -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor/supervisor.conf
-cp -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor.conf
-service supervisor start
+#!/bin/sh
+
+#	way to stop and start supervisord
+
+#	# stop supervisord in daemon mode
+#	service supervisor stop
+#	# fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
+#	unlink /var/run/supervisor.sock 2> /dev/null
+#	unlink /tmp/supervisor.sock 2> /dev/null
+#	# start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
+#	echo "Starting supervisord in foreground ..."
+#	service supervisor start
+
+# run this to keep the container running ..as the container needs a foreground task to run to keep it alive 
+tail -100f /var/log/supervisor/supervisord.log
