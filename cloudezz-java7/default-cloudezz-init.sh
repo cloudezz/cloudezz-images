2e4687deacc280da19663957620909768b5e3f7d
tanmally <tm@beyondbytes.co.in>
2014-03-22 18:56:30 +0530

    replace mv with cp

--- cloudezz-java7/default-cloudezz-init.sh
+++ cloudezz-java7/default-cloudezz-init.sh
@@ -7,6 +7,6 @@ unlink /var/run/supervisor.sock 2> /dev/null
 unlink /tmp/supervisor.sock 2> /dev/null
 # start supervisord in non daemon mode as it has to keep the machine running else it will shutdown
 echo "Starting supervisord in foreground ..."
-mv -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor/supervisor.conf
-mv -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor.conf
+cp -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor/supervisor.conf
+cp -f /opt/cloudezz-config/supervisorND.conf /etc/supervisor.conf
 service supervisor start
