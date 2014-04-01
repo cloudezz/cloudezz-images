f5098dccdc63e65edc47453e9cb6d778cd7babb6
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-04-01 16:06:52 +0530

    Adding serf join.py

--- cloudezz-base/start-serf.sh
+++ cloudezz-base/start-serf.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # start serf with a node name, role and other values to start and join a cluster
-exec serf agent -bind 0.0.0.0:7946 -node=$SERF_NODE_NAME -encrypt=$SERF_CLUSTER_KEY  -tag cluster_id=$SERF_CLUSTER_ID  -tag role=$SERF_ROLE  -tag is_service=$SERVICE_OR_NOT  -tag expose_default_port=$DEFAULT_PORT_TO_EXPOSE 
+exec serf agent -bind 0.0.0.0:7946 -node=$SERF_NODE_NAME -encrypt=$SERF_CLUSTER_KEY  -tag cluster_id=$SERF_CLUSTER_ID  -tag role=$SERF_ROLE  -tag is_service=$SERVICE_OR_NOT  -tag expose_default_port=$DEFAULT_PORT_TO_EXPOSE
+
+exec python serfjoin.py 
 
 
 
