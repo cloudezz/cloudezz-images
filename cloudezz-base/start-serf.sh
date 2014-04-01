728ab82adc7cbb5da411372e654c0e361c00f000
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-04-01 18:30:22 +0530

    added logs

--- cloudezz-base/start-serf.sh
+++ cloudezz-base/start-serf.sh
@@ -4,7 +4,8 @@ touch /opt/cloudezz-config/output.txt
 # start serf with a node name, role and other values to start and join a cluster
 exec serf agent -bind 0.0.0.0:7946 -event-handler "member-join=/opt/cloudezz-config/join.sh" -event-handler "member-leave,member-failed=/opt/cloudezz-config/leave.sh" -node=$SERF_NODE_NAME -encrypt=$SERF_CLUSTER_KEY  -tag cluster_id=$SERF_CLUSTER_ID  -tag role=$SERF_ROLE  -tag is_service=$SERVICE_OR_NOT  -tag expose_default_port=$DEFAULT_PORT_TO_EXPOSE >> /opt/cloudezz-config/output.txt
 
-exec python serfjoin.py
+touch /opt/cloudezz-config/py_output.txt
+exec python /opt/cloudezz-config/serfjoin.py >> py_output.txt
 
 
 
