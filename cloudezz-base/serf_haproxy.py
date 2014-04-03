8820c1f8d4724d3c1932cc39d5afe360383bce3b
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-04-03 13:56:47 +0530

    Fixed HAProxy config editing

--- cloudezz-base/serf_haproxy.py
+++ cloudezz-base/serf_haproxy.py
@@ -1,7 +1,7 @@
 import os;
 import socket;
 import json;
-import os.path
+import os;
 import linecache
 
 from random import randrange
@@ -16,11 +16,11 @@ def file_len(filename, lookup):
     with open(filename) as myFile:
         for num, line in enumerate(myFile, 1):
             line_offset.append(offset)
-            offset += len(line)
+            offset += len(line.expandtabs())
             if lookup in line:
-                print num
+                print num, line_offset
                 return num, line_offset[num-1]
-    return -1
+    return -1,-1
             
 def main(json_inp):
 #     serf_members_out = local('serf members -tag is_service=true -status=alive -format=json');
@@ -38,39 +38,42 @@ def main(json_inp):
                  if os.path.isfile(tmp_filename):
                      f = open(tmp_filename, 'r+')
                      linenum, offset = file_len(tmp_filename, '##Stop '+member_role)
-                     print linenum, offset
                      if(linenum<=0):
-                         f.write('##Start '+member_role+'\n');
+                         f.seek(0,2);
+                         f.write('\n##Start '+member_role);
                          for ports in member_exposed_ports_array:
                              lxc_port,  host_port = ports.split(':');
-                             f.write('listen '+member_role+'-'+lxc_port);
+                             f.write('\nlisten '+member_role+'-'+lxc_port);
                              f.write('\n\tbind *:' + lxc_port)
-                             f.write('\n\tmode tcp \n\tbalance roundrobin');
+                             f.write('\n\tmode tcp\n\tbalance roundrobin');
                              f.write('\n\tserver ' + member_role+'-'+str(randrange(10))+ ' '+member_ip+':'+host_port)
-                             f.write('\n');
-                         f.write('##Stop '+member_role+'\n');
+                         f.write('\n##Stop '+member_role);
                          f.close();
                      else:
-                         f.seek(offset-1,0);
+                         old = f.read();
+                         f.seek(0,0);
+                         f.write(old);
+                         f.seek(0-len('\n##Stop '+member_role)-1,1)
                          for ports in member_exposed_ports_array:
                              lxc_port,  host_port = ports.split(':');
                              f.write('\n\tserver ' + member_role+'-'+str(randrange(10))+ ' '+member_ip+':'+host_port)
+                         f.write('\n##Stop '+member_role);
                          f.close();
                  else:
                      f = open(tmp_filename, 'w')
-                     f.write('##Start '+member_role+'\n');
+                     f.write('##Start '+member_role);
                      for ports in member_exposed_ports_array:
                          lxc_port,  host_port = ports.split(':');
-                         f.write('listen '+member_role+'-'+lxc_port);
+                         f.write('\nlisten '+member_role+'-'+lxc_port);
                          f.write('\n\tbind *:' + lxc_port)
-                         f.write('\n\tmode tcp \n\tbalance roundrobin');
+                         f.write('\n\tmode tcp\n\tbalance roundrobin');
                          f.write('\n\tserver ' + member_role+'-'+str(randrange(10))+ ' '+member_ip+':'+host_port)
-                         f.write('\n');
-                     f.write('##Stop '+member_role+'\n');
+                     f.write('\n##Stop '+member_role);
                      f.close();
 
+# json1 = '{"members": [     {      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:45954",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    }  ]}';  
+json1 = '{"members": [   {      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:45954",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    },  {      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:49154",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    } ,    {      "name": "d344680ad46b",      "addr": "172.17.0.3:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:45098",        "expose_default_port": "",        "host_ip": "10.1.1.1",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "failed",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    },{      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "100:900,200:901,300:902",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "rabbitmq",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    } ]}';  
 
-# json1 = '{"members": [    {      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "100:900,200:901,300:902",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "rabbitmq",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    }  ]}';  
-json1 = '{"members": [    {      "name": "db4c20f0ac53",      "addr": "172.17.0.2:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:49154",        "expose_default_port": "",        "host_ip": "10.0.2.2",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "alive",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    } ,    {      "name": "d344680ad46b",      "addr": "172.17.0.3:7946",      "port": 7946,      "tags": {        "cluster_id": "",        "expose_default_host_port": "3306:45098",        "expose_default_port": "",        "host_ip": "10.1.1.1",        "is_service": "false",        "role": "mysql",        "serf_host_port": ""      },      "status": "failed",      "protocol": {        "max": 4,        "min": 2,        "version": 4      }    } ]}';  
-
+if os.path.isfile(tmp_filename):
+    os.remove(tmp_filename)
 main(json1);
