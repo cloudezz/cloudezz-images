d322112a9fca5b7d921bf9ec6c846595f2eebb28
Dhanush Gopinath <dhanush@beyondbytes.co.in>
2014-04-04 13:04:46 +0530

    added a wait for python execution finish

--- cloudezz-base/join.sh
+++ cloudezz-base/join.sh
@@ -13,10 +13,16 @@ fi
 echo "Member Joining to LB" >> join_output.txt
 
 rm /tmp/listen.cfg
+echo "Python Execution Begin" >> join_output.txt
 
-exec python serf_haproxy.py
+exec python serf_haproxy.py > /dev/null 2>&1 &
 
+wait %2
+
+echo "Python Execution End" >> join_output.txt
 
 cat /opt/cloudezz-config/haproxy.cfg.base /tmp/listen.cfg > /opt/cloudezz-config/haproxy.cfg
+
+echo "Copied HAProxy files" >> join_output.txt
 
 supervisorctl restart haproxy
