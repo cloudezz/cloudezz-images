f2b430b90d2191c36f0248f1b4bc99fbdb98501d
tanmally <tm@beyondbytes.co.in>
2014-04-05 00:04:28 +0530

    Merged with base to bring in serf modifications

--- cloudezz-service-base/start.sh
+++ cloudezz-service-base/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # restart Fail2Ban service to avoid dos attack thru ssh
 echo "Starting fail2ban service"
 service fail2ban restart >/dev/null
@@ -12,17 +11,40 @@ echo "Starting shellinabox service"
 service shellinabox reload >/dev/null
 fi
 
+# print the private key or ssh password on console so that the user can connect thru ssh
+/opt/cloudezz-config/ssh-key-init.sh
 
-
-# print the private key on console so that the user can connect thru ssh
-/opt/ssh-key-init.sh
-
-# run init.sh file if available 
-if [ -e "/opt/init.sh" ]
+if [ $GIT_URL ]
 then
- chmod +x /opt/init.sh
- /opt/init.sh
+rm -r /cloudezz/app 
+git clone $GIT_URL /cloudezz/app >/dev/null
 fi
 
-# start supervisord in nodaemon mode
-supervisord -n -c /etc/supervisor.conf
+#write environment variables
+
+env | grep _ >> /etc/environment
+ 
+# start supervisord in daemon mode
+# fix based on http://stackoverflow.com/questions/14479894/stopping-supervisord-shut-down
+unlink /var/run/supervisor.sock 2> /dev/null
+unlink /tmp/supervisor.sock 2> /dev/null
+service supervisor start
+
+
+if [ -e "/cloudezz/app/cloudezz-config/cloudezz-init.sh" ]
+then
+ echo "Started cloudezz init script..." 
+ chmod +x /cloudezz/app/cloudezz-config/cloudezz-init.sh
+ /cloudezz/app/cloudezz-config/cloudezz-init.sh
+else
+	if [ -e "/opt/cloudezz-config/default-cloudezz-init.sh" ]
+	then
+		echo "Started default cloudezz init script..." 
+		chmod +x /opt/cloudezz-config/default-cloudezz-init.sh
+		/opt/cloudezz-config/default-cloudezz-init.sh
+	fi	
+fi
+
+
+
+
